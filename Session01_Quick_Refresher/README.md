12ca59e9512104087d4d8e97022838cac856a89d
abdksyed <abdksyed@gmail.com>
2021-01-02 15:38:06 +0530

    Assignment 1

---
+++ Session01_Quick_Refresher/README.md
@@ -0,0 +1,6 @@
+# Session 1 - Quick Refresher
+
+### Class Notebook - 
+## Assignment Objective - CI/CD Practice
+
+The Assignment is to Test 
---
+++ Session01_Quick_Refresher/session1.py
@@ -0,0 +1,13 @@
+def myfunc():
+    string = 'Cppsecrets'
+    n = len(string)
+    arr = []
+    for i in range(n):
+        for j in range(i+1, n+1):
+            a = string[i:j]
+            arr.append(a)
+            print(arr)
+
+
+def my_func():
+    pass
